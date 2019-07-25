af731045febe6a0bffe695047a47d2805172bae2
baalajimaestro <baalajimaestro@computer4u.com>
2019-07-25 18:06:55 +0530

    Dot is very important
    Signed-off-by: baalajimaestro <baalajimaestro@computer4u.com>

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM baalajimaestro/android_build:latest
 
+COPY . .
 RUN bash runner.sh
 USER ci
 WORKDIR /home/ci
--- sync_pe.sh
+++ sync_pe.sh
@@ -1,4 +1,5 @@
 cd
 repo init -u https://github.com/PixelExperience/manifest -b pie --depth 1
-repo sync --force-sync --current-branch --no-tags --no-clone-bundle --optimized-fetch --prune -j$(nproc) -q > sync.log 2>&1
+echo "Sync starting!"
+repo sync --force-sync --current-branch --no-tags --no-clone-bundle --optimized-fetch --prune -j$(nproc) -q
 echo "Synced PE trees successfully!"
